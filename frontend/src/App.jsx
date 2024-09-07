741f305f194e8f592a4091e5ad7185297ce79934
Mohit Raj Rathor <mohitrajrathor@gmail.com>
2024-09-07 23:55:10 +0530

    tailwindcss configuration setup completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,9 +7,9 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <h1 className="text-3xl text-center font-bold underline">
+      Hello world!
+    </h1>
   );
 }
 
